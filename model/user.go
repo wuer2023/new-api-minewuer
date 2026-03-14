c4b9bee9794fddca78b2fbd8058b568e02eb6074
wuer2023 <wuer2023@users.noreply.github.com>
2026-03-14 15:17:21 +0800

    fix: ensure InviterId is properly saved during OAuth registration

--- model/user.go
+++ model/user.go
@@ -391,6 +391,7 @@ func (user *User) Insert(inviterId int) error {
 	user.Quota = common.QuotaForNewUser
 	//user.SetAccessToken(common.GetUUID())
 	user.AffCode = common.GetRandomString(4)
+	user.InviterId = inviterId
 
 	// 初始化用户设置，包括默认的边栏配置
 	if user.Setting == "" {
@@ -449,6 +450,7 @@ func (user *User) InsertWithTx(tx *gorm.DB, inviterId int) error {
 	}
 	user.Quota = common.QuotaForNewUser
 	user.AffCode = common.GetRandomString(4)
+	user.InviterId = inviterId
 
 	// 初始化用户设置
 	if user.Setting == "" {
